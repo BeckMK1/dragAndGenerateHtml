cfe8c3a471d8fbabb82977c20073caea2ed7ca6a
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2022-11-09 14:00:29 +0100

    started

---
+++ vue-app/src/components/BlockLibCom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-48 h-screen border-l float-right border-black">
+    <div class="block1 libBlock w-screen h-96 bg-black ml-auto mr-auto mt-3">
+        <div class="header w-full bg-red-600 h-6"></div>
+        <p >test test test test test</p>
+        <div class="footer w-full bg-slate-800 h-6"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.libBlock{
+width: 150px !important;
+height: 100px !important;
+}
+</style>
---
+++ vue-app/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <block-lib-com></block-lib-com>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BlockLibCom from '@/components/BlockLibCom.vue';
+export default {
+  components: { BlockLibCom },
+  name: 'HomeView',
+}
+</script>
